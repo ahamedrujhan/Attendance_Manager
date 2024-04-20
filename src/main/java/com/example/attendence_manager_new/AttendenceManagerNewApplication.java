7ee12f5fd4150ac9899353880a89e262674e76ac
Ahamed Rujhan <rujhanbheem123@gmail.com>
2024-04-20 12:42:44 +0530

    Intialized Springboot

---
+++ src/main/java/com/example/attendence_manager_new/AttendenceManagerNewApplication.java
@@ -0,0 +1,13 @@
+package com.example.attendence_manager_new;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttendenceManagerNewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AttendenceManagerNewApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/attendence_manager_new/AttendenceManagerNewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.attendence_manager_new;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttendenceManagerNewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
